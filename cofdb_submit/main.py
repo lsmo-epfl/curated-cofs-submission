9f8ad09ca6b07f232665b893b1eb53d3a13b7628
Daniele Ongari <daniele.ongari@gmail.com>
2020-01-13 18:13:23 +0100

    improve paper and cif info extraction

--- cofdb_submit/main.py
+++ cofdb_submit/main.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
-import panel as pn
+"""Add new entries to the CURATED-COFs files: cof-papers.csv and cof-frameworks.csv."""
 
-"""Check consistency of curated COF database"""
+import os
+import panel as pn
 import pandas
 import datetime
-import os
+
 CURATED_COFS=os.environ.get('CURATED_COFS', os.path.abspath('./CURATED-COFs'))
 PAPER_FILE = os.path.join(CURATED_COFS, 'cof-papers.csv')
 FRAMEWORKS_FILE = os.path.join(CURATED_COFS, 'cof-frameworks.csv')
 CIFS_FOLDER = os.path.join(CURATED_COFS, 'cifs')
 
-def parse_cofs_id(id):
-    return {
-        'year': id[1:3],
-        'counter': int(id[3:5]),
-    }
-
-def mint_paper_id():
+def mint_paper_id(doi, year):
+    """Check if the paper is already in cof-papers.csv (same DOI) and print that value,
+    otherwise assign the new paper ID.
+    """
     df_papers = pandas.read_csv(PAPER_FILE)
-    last_id = sorted(df_papers['CURATED-COFs paper ID'])[-1]
-    year = str(datetime.datetime.now().year)
+    if doi in df_papers['DOI'].values:
+        return str(df_papers[df_papers['DOI'] == doi]['CURATED-COFs paper ID'].values[0]) + " (already present)"
 
-    last_dict = parse_cofs_id(last_id)
-
-    #import pdb; pdb.set_trace()
-    if last_dict['year'] == year[2:]:
-        counter = last_dict['counter'] + 1
-    else:
+    id_year = sorted(df_papers[df_papers['CURATED-COFs paper ID'].str.contains("p"+year[2:])]['CURATED-COFs paper ID'])
+    if len(id_year)==0:
         counter = 0
+    else:
+        last_id_year = id_year[-1]
+        last_counter_year = int(last_id_year[3:5])
+        counter = last_counter_year + 1
 
     return "p{:s}{:02d}".format(year[2:], counter)
 
@@ -35,13 +33,8 @@ def mint_cof_id(paper_id, charge, dimensionality):
     df_frameworks = pandas.read_csv(FRAMEWORKS_FILE)
     other_cofs = [ id for id in df_frameworks["CURATED-COFs ID"] if id.startswith(paper_id[1:])]
     counter = len(other_cofs)
-
     return "{paper}{counter}{charge}{dim}".format(paper=paper_id[1:], counter=str(counter), charge=charge,
                                                   dim=dimensionality)
-
-"""Add paper info"""
-"CURATED-COFs paper ID","Reference","URL","Title"
-"p0500","Science, 2005, 310, 1166-1170","http://science.sciencemag.org/content/310/5751/1166.short","Porous, crystalline, covalent organic frameworks"
 
 pn.extension()
 
@@ -52,7 +45,6 @@ inp_reference = pn.widgets.TextInput(name='Publication reference', placeholder='
 inp_title = pn.widgets.TextInput(name='Publication title', placeholder='')
 inp_year = pn.widgets.TextInput(name='Publication year', placeholder='')
 inp_paper_id = pn.widgets.TextInput(name='CURATED-COFs paper ID', placeholder='')
-inp_url = pn.widgets.TextInput(name='Publication URL', placeholder='')
 
 div_out = pn.widgets.StaticText(name='Output', value='')
 btn_add_paper = pn.widgets.Button(name='Add paper', button_type='primary')
@@ -65,8 +57,10 @@ def get_metadata(doi):
     return json.loads(response.content)
 
 def on_click_fetch(event):
+    """Get metadata for DOI, and return an error if the DOI is not valid (no metadata found)."""
     import dateutil
     from crossref.restful import Works
+    import json
 
     # test data
     inp_doi.value = inp_doi.value or "10.1021/jacs.9b01891"
@@ -74,31 +68,50 @@ def on_click_fetch(event):
     works = Works()
     print("Querying Crossref API for doi {}".format(inp_doi.value))
     metadata = works.doi(inp_doi.value)
-    inp_title.value = str(metadata['title'][0])
+    #print(json.dumps(metadata,sort_keys=True, indent=4))
 
-    year = str(dateutil.parser.parse(metadata['indexed']['date-time']).year)
+    if not metadata:
+        btn_doi.button_type = 'danger'
+        inp_title.value = inp_year.value = inp_reference.value = inp_paper_id.value = "ERROR: wrong/missing DOI."
+        return
+
+    inp_title.value = str(metadata['title'][0])
+    journal = str(metadata['short-container-title'][0])
+
+    if 'volume' in metadata: # already published in an issue
+        volume = metadata['volume']
+        if 'published-print' in metadata: # ACS, wiley
+            year = str(metadata['published-print']['date-parts'][0][0])
+        elif 'created' in metadata: # RSC
+            year = str(metadata['created']['date-parts'][0][0])
+        else:
+            year = 'ERROR: year not found.'
+    else: # not yet in an issue: assuming that it will be published at the same year of today
+        volume = "xxxxx"
+        year = str(datetime.datetime.now().year)
+
     inp_year.value = year
 
-    journal = str(metadata['container-title'][0])
-    inp_reference.value = "{}, {}, {}, {}".format(journal, year, metadata['volume'], metadata['page'])
-
-    inp_url.value = "https://doi.org/" + inp_doi.value
-
-    inp_paper_id.value = mint_paper_id()
+    if 'page' in metadata: # most of the journals
+        inp_reference.value = "{}, {}, {}, {}".format(journal, year, volume, metadata['page'])
+    else: # NatComm or not yet in an issue
+        inp_reference.value = "{}, {}, {}".format(journal, year, volume)
+    inp_paper_id.value = mint_paper_id(doi=inp_doi.value, year=inp_year.value)
+    btn_doi.button_type = 'success'
 
 btn_doi.on_click(on_click_fetch)
 
-
 def on_click_add(event):
-    """Add paper to list"""
-    if not (inp_paper_id.value and inp_reference.value and inp_url.value and inp_title.value):
+    """Add paper to file cof-papers.csv: botton will turn red if something is missing or paper already present."""
+    if not (inp_paper_id.value and inp_reference.value and inp_doi.value and inp_title.value) or \
+       "(already present)" in inp_paper_id.value:
         btn_add_paper.button_type = 'danger'
         return
 
     btn_add_paper.button_type = 'primary'
 
-    line = '"{id}","{ref}","{url}","{title}"\n'.format(id=inp_paper_id.value, ref=inp_reference.value,
-                                                  url=inp_url.value, title=inp_title.value)
+    line = '{id},"{ref}",{doi},"{title}"\n'.format(id=inp_paper_id.value, ref=inp_reference.value,
+                                                  doi=inp_doi.value, title=inp_title.value)
     print(line)
     with open(PAPER_FILE, 'a+') as handle:
         handle.write(line)
@@ -113,7 +126,6 @@ column = pn.Column(
     inp_title,
     inp_year,
     inp_reference,
-    inp_url,
     inp_paper_id,
     btn_add_paper,
 )
@@ -128,27 +140,7 @@ class CifForm():
     """Input form for CIF file"""
 
     def __init__(self):
-
-        # Tested this with panel 0.7.0a14, but loading the app was very slow
-        # inp_cif = pn.widgets.input.TextAreaInput(name='CIF', value="""
-        # data_crystal
-        #
-        # _cell_length_a
-        # _cell_length_b
-        # _cell_length_c
-        # _cell_angle_alpha
-        # _cell_angle_beta
-        # _cell_angle_gamma
-        #
-        # _symmetry_space_group_name_H-M  'P 1'
-        #
-        # loop_
-        # _atom_site_label
-        # _atom_site_fract_x
-        # _atom_site_fract_y
-        # _atom_site_fract_z
-        # """)
-        self.inp_cif = pn.widgets.FileInput(name='CIF')
+        self.inp_cif = pn.widgets.FileInput(name='CIF', accept='.cif')
         self.btn_cif = pn.widgets.Button(name='Parse CIF', button_type='primary')
         self.btn_cif.on_click(self.on_click_parse)
 
@@ -157,12 +149,12 @@ class CifForm():
         self.jsmol_script_source = bmd.ColumnDataSource()
         self.applet = structure_jsmol(self.jsmol_script_source)
 
-        self.inp_source = pn.widgets.Select(name='CIF source', options={ 'Supporting information': 'SI', 'CSD': 'CSD', 'Tong database': 'tong-v2' })
+        self.inp_source = pn.widgets.Select(name='CIF source', options={'SI': 'SI', 'SI (CIF)': 'SI (CIF)', 'CSD': 'CSD'})
         self.inp_name = pn.widgets.TextInput(name='CIF name', placeholder='As used in publication')
-        self.inp_dimensionality = pn.widgets.Select(name='CIF dimensionality', options={ '2D': '2D', '3D': '3D' })
+        self.inp_dimensionality = pn.widgets.TextInput(name='CIF dimensionality', placeholder='Detected by ASE')
         self.inp_elements = pn.widgets.TextInput(name='CIF elements', placeholder='C,H,...')
         self.inp_duplicate = pn.widgets.TextInput(name='CIF duplicate found', value='none')
-        self.inp_modifications = pn.widgets.TextInput(name='CIF modifications', placeholder='xy alignment, replicated along z, ...')
+        self.inp_modifications = pn.widgets.TextInput(name='CIF modifications', value='none')
         self.inp_charge = pn.widgets.Select(name='CIF charge', options={ 'Neutral': 'N', 'Charged': 'C' })
         self.inp_cof_id = pn.widgets.TextInput(name='COF ID', value='none')
         self.btn_mint_id = pn.widgets.Button(name='Mint', button_type='primary')
@@ -202,6 +194,9 @@ class CifForm():
         return self.column.servable()
 
     def on_click_parse(self, event):
+        """Load the CIF, unwrap it to P1 using ASE, extract some info, and display it.
+        NOTE: it would be nice to use the filename as CIF name, but FileInput doesn't allow you to know the path.
+        """
         from ase.io import read, write
         from ase.io.cif import write_cif
         from ase.geometry.dimensionality import analyze_dimensionality
@@ -215,17 +210,22 @@ class CifForm():
         formula = atoms.get_chemical_formula()
         elements = [e for e in re.split(r'\d+', formula) if e]
         self.inp_elements.value = ",".join(elements)
+        self.inp_modifications.value = 'none'
 
         intervals = analyze_dimensionality(atoms, method='RDA')
-        if intervals[0].dimtype == '2D' or True:
+        if intervals[0].dimtype == '2D':
             self.inp_dimensionality.value = '2D'
 
-            # multiply cell if cell vectors are too short
+            # Check if it is correcly oriented, and extend to two layers if onyly one is present
+            z_min_thr = 6 #if less, it is likely a single layer
             cell_lengths = atoms.get_cell_lengths_and_angles()[0:3]
-            if any(cell_lengths < 4.5):
+            if min(cell_lengths[0],cell_lengths[1] < z_min_thr): # X or Y are perpendicular to a single layer
+                self.inp_dimensionality.value = "ERROR: you need to rotate the axes to have the layers on XY."
+                self.inp_modifications.value = "ERROR: you need to rotate the axes to have the layers on XY."
+            if cell_lengths[2] < z_min_thr: # Z is perpendicular to a single layer
                 import numpy as np
-                p = np.diag([2 if l < 4.5 else 1 for l in cell_lengths  ])
-                atoms = make_supercell(atoms, p)
+                atoms = make_supercell(atoms, np.diag([1,1,2]))
+                self.inp_modifications.value = 'replicated 2x in C direction'
         else:
             self.inp_dimensionality.value = '3D'
 
@@ -248,8 +248,9 @@ class CifForm():
             return
 
         self.btn_add_cif.button_type = 'primary'
-        line = '"{id}","{source}","{name}","{elements}","{duplicates}","{mod}"\n'          .format(id=info['cof_id'], source=info['source'], name=info['name'], elements = info['elements'],
-                 duplicates=info['duplicate'], mod=info['modifications'])
+        line = '{id},{source},"{name}","{elements}","{duplicates}","{mod}"\n'.format(
+            id=info['cof_id'], source=info['source'], name=info['name'], elements = info['elements'],
+            duplicates=info['duplicate'], mod=info['modifications'])
         print(line)
         with open(FRAMEWORKS_FILE, 'a+') as handle:
            handle.write(line)
@@ -258,13 +259,25 @@ class CifForm():
 
         print("Writing {}".format(cif_path))
         write(cif_path, self.atoms)
-        #with open(cif_path, 'wb') as handle:
-        #    handle.write(self.inp_cif.value)
+
+        # Using manage_crystal to use the standard formatting
+        from manage_crystal.file_parser import parse_from_filepath
+        from manage_crystal.file_writer import write_to_filepath
+        crys = parse_from_filepath(cif_path, 0)
+        crys.check_parse()
+        crys.compute_atom_count()
+        write_to_filepath(crys, cif_path)
 
         self.btn_add_cif.button_type = 'success'
 
     def mint_framework_id(self, event):
-        self.inp_cof_id.value = mint_cof_id(inp_paper_id.value, self.inp_charge.value, self.inp_dimensionality.value[0])
+        """Get the paper id, and warn if not paper info is given."""
+        if not inp_paper_id.value:
+            self.inp_cof_id.value = "ERROR: add DOI of the paper first."
+            return
+        # Remove "(already present)" if present
+        paper_id = inp_paper_id.value.split()[0]
+        self.inp_cof_id.value = mint_cof_id(paper_id, self.inp_charge.value, self.inp_dimensionality.value[0])
 
     def display(self, cif_str):
         """Update applet to show CIF."""
